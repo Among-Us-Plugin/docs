d3dcf1ae402f67c9d5e0b4b731d6f47d630079a2
pairnon <janpairnon@gmail.com>
2024-05-06 10:07:42 -0700

    Add AUP branding

--- docs/docs-home.md
+++ docs/docs-home.md
@@ -3,4 +3,6 @@ slug: /
 sidebar_position: 1
 ---
 
-# Docs Home
+# AUP Docs Home
+
+---
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/crewmate.ico',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
@@ -61,10 +61,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'AUP Docs',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'AUP Logo',
+          src: 'img/crewmate.png',
         },
         items: [
           // {
